a73c6d190855ec204ad20218da01165cc1d87fa0
Rafael <rafamoresi@gmail.com>
2022-05-22 12:43:15 -0300

    added post method for employee
    fixed class reader helper
    
    update gitignore file

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
+using Repository.RepositoryInterfaces;
+using Repository;
+using Assets;
 
 namespace backend
 {
@@ -19,7 +22,11 @@ namespace backend
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IClassReader, ClassReader>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             
+            services.AddDbContext<AppDbContext>();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
